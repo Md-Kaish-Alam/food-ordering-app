a13bc2fbdd47423c07109a43c34989a7ef94adb2
Md Kaish Alam <alamkaishg1511@gmail.com>
2024-02-23 18:38:22 +0530

    build: database setup and connection completed.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
+  console.log("Connected to Database");
+});
 
 const PORT = 5000;
 
@@ -14,4 +19,4 @@ app.get("/test", async (req: Request, res: Response) => {
 
 app.listen(PORT, () => {
   console.log(`Server Started at localhost, \t http://localhost:${PORT}`);
-})
+});
