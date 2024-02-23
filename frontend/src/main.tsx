eaf84aab56809cddb9dc3a14866a9465a4a11709
Md Kaish Alam <alamkaishg1511@gmail.com>
2024-02-23 19:03:34 +0530

    build: layout and header components completed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
 
+import "./global.css";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
